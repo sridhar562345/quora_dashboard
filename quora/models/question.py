530ca821b5f3c500a274458ab6db5c4879248ff6
Sridhar <sridhar562345@gmail.com>
2025-04-08 19:15:40 +0530

    update models

--- quora/models/question.py
+++ quora/models/question.py
@@ -5,3 +5,4 @@ from django.contrib.auth.models import User
 class Question(models.Model):
     text = models.TextField()
     posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
